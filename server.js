48f432efbb2e9f2f5192a84c9f60f945a2f8eb85
yashikabijoliya <84142584+yashikabijoliya@users.noreply.github.com>
2021-05-17 23:11:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("Express");
+const bodyparser=require("body-parser");
+
+const app = express();
+app.use(bodyparser.urlencoded({extended: true}))
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+  console.log(req.body.num1);
+  var result= n1+n2;
+  res.send("the answer is "+ result);
+});
+app.listen(3000,function(){
+  console.log("server has started on port 3000");
+});
